49b1fa8bc634098aeed2c30ebe6335696ad427f9
Chloe <fernandez.chloe.pro@outlook.fr>
2018-10-11 14:47:36 +0200

    hangar

---
+++ Hangar.java
@@ -0,0 +1,10 @@
+class Hangar {
+    public static void main (String[] args){
+        Boat koinkoin = new Boat("koinkoin");
+        Car mini = new Car("mini");
+        System.out.println(koinkoin.doStuff());
+        System.out.println(mini.doStuff());
+    
+    }
+    
+}
